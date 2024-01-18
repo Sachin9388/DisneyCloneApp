20ad645471b5eba23fcca8066903f0f56904c7c5
kirtan <sonagrakirtan@gmail.com>
2024-01-18 15:46:02 +0530

    kirtan

--- src/app/Component/Pages/Movies/Movies.js
+++ src/app/Component/Pages/Movies/Movies.js
@@ -30,35 +30,32 @@ export const Movies = () => {
   return (
     <>
       <div className="flex items-center  w-[100%] justify-center">
-        <div className="">
+        <div className="flex mt-5 bg-[#14161d] mb-5 ml-20 ">
           <a
             href="/"
             class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
           >
             <img
-              className="object-cover w-full gap-2 space-x-3 rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+              className="object-cover  w-4 rounded-t-lg  md:h-48 md:w-96 md:rounded-none md:rounded-s-lg"
               src="/SVG/download (2).jpeg"
               alt=""
-            />
+            /></a>
             <div class="flex flex-col justify-between p-4 leading-normal">
               <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                 Noteworthy technology acquisitions 2021
               </h5>
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                Here are the biggest enterprise technology acquisitions of 2021
-                so far, in reverse chronological order.
-              </p>
+              <button className="bg-gray-600 w-fit px-5 rounded-md place-self-start text-white inline-block" >Trailer</button>
             </div>
-          </a>
+          
         </div>
 
-        {/* <div className="flex ">
+        <div className="flex ">
           <img
             className="w-44 h-40 rounded-md "
             src={photos[currentIndex]}
             alt={`Movie ${currentIndex}`}
           />
-        </div> */}
+        </div>
       </div>
       <div>
         <div className="flex pl-44 ">
